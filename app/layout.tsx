a64ab29b0618f6544990d04e5365d78576de3751
ibaker72 <Bakereyad7@gmail.com>
2026-05-07 15:41:31 +0000

    app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={inter.variable}>
-      <body className="min-h-screen flex flex-col bg-[#FAF8F3]">
+      <body className="min-h-screen flex flex-col bg-[#FAF8F3]" suppressHydrationWarning>
         <ToastProvider>
           <CartProvider>
             <Header />
